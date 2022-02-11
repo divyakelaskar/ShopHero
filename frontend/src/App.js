bf8d31333c9900e3612e2bd553a9827bbec434fc
Divya Kelaskar <divyakelaskar@gmail.com>
2022-02-11 21:01:29 +0530

    feat: linked the pages to the app ✨

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+
+//Screens
+import HomeScreen from './screens/HomeScreen';
+import CartScreen from './screens/CartScreen';
+import ProductScreen from './screens/ProductScreen';
+
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <Router>
-      
+      <Navbar/>
       <main>
-        <Switch>
-          <Route exact path="/" component={HomeScreen}/>
-          <Route  exact path="/product/:id" component={ProductScreen}/>
-          <Route  exact path="/cart" component={CartScreen}/>
-        </Switch>
+        <Routes>
+          <Route exact path="/" component={HomeScreen} />
+          <Route  exact path="/product/:id" component={ProductScreen} />
+          <Route  exact path="/cart" component={CartScreen} />
+        </Routes>
       </main>
       
     </Router>
