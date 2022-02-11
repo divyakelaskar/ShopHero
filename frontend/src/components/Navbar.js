2d12c327a26bb2def00f3bbf47645efa7a200937
Divya Kelaskar <divyakelaskar@gmail.com>
2022-02-11 20:57:39 +0530

    feat: added cart and hamburger menu to navbar ✨

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -13,10 +13,18 @@ const Navbar = () => {
         <ul className="navbar__links">
             <li>
                 <Link to="/cart">
-                
+                  <i className='fas fa shopping-cart'></i>
+                  Cart
+                  <span className='cartlogo__badge'>0</span>                
                 </Link>
             </li>
         </ul>
+
+        <div className='hamburger__menu'>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
     </nav>
   )
 }
