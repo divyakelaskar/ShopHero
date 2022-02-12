a9fd1a6204eafb4048042f80393843f6c102e8d8
Divya Kelaskar <divyakelaskar@gmail.com>
2022-02-12 20:45:49 +0530

    fix: shopping cart icon issue 🐛

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -6,16 +6,19 @@ const Navbar = () => {
     <nav className="navbar">
         {/* title */}
         <div className="navbar__title">
+          <Link to="/" style={{ textDecoration: 'none' }}>
             <h2>ShopHero</h2>
+          </Link>
         </div>
 
         {/* links */}
         <ul className="navbar__links">
             <li>
-                <Link to="/cart">
-                  <i className='fas fa shopping-cart'></i>
-                  Cart
-                  <span className='cartlogo__badge'>0</span>                
+                <Link to="/cart" className='cart__link'>
+                <i class="fa-solid fa-cart-shopping"></i>
+                  <span>Cart
+                    <span className='cartlogo__badge'>0</span>
+                  </span>               
                 </Link>
             </li>
         </ul>
