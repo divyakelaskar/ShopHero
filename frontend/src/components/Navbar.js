d58c60ee57a098ddf6d58555b94c20a48d1ec350
Divya Kelaskar <divyakelaskar@gmail.com>
2022-02-14 20:34:52 +0530

    feat: made cart item count badge dynamic ✨

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,7 +1,14 @@
-import './Navbar.css';
-import { Link } from 'react-router-dom';
+import "./Navbar.css";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
-const Navbar = ({click}) => {
+const Navbar = ({ click }) => {
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+
+  const getCartCount = () => {
+    return cartItems.reduce((qty, item) => Number(item.qty) + qty, 0);
+  };
   return (
     <nav className="navbar">
         {/* title */}
@@ -17,7 +24,7 @@ const Navbar = ({click}) => {
                 <Link to="/cart" className='cart__link'>
                 <i className="fa-solid fa-cart-shopping"></i>
                   <span>Cart
-                    <span className='cartlogo__badge'>0</span>
+                    <span className='cartlogo__badge'>{getCartCount()}</span>
                   </span>               
                 </Link>
             </li>
