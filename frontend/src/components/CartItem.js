1df8c9d7db6fb1effdb1dd41b82a43201206b867
Divya Kelaskar <divyakelaskar@gmail.com>
2022-02-14 20:29:33 +0530

    feat : modified cart item component ✨

--- frontend/src/components/CartItem.js
+++ frontend/src/components/CartItem.js
@@ -1,27 +1,35 @@
-import './CartItem.css';
+import "./CartItem.css";
 import { Link } from "react-router-dom";
 
-const CartItem = () => {
+const CartItem = ({ item, qtyChangeHandler, removeHandler }) => {
   return (
     <div className="cartitem">
       <div className="cartitem__image">
-        <img src="https://images.unsplash.com/photo-1519183071298-a2962feb14f4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"/>
+        <img src={item.imageUrl} alt={item.name} />
       </div>
-      <Link to={`/product/${1111}`} className="cartItem__name">
-        <p>Product 1</p>
+      <Link to={`/product/${item.product}`} className="cartItem__name">
+        <p>{item.name}</p>
       </Link>
-      <p className="cartitem__price">Rs. 4586/-</p>
-      <select>
-                  <option value={1}>1</option>
-                  <option value={2}>2</option>
-                  <option value={3}>3</option>
-                  <option value={4}>4</option>
-                </select>
-      <button className="cartItem__deleteBtn">
+      <p className="cartitem__price">₹ {item.price} /-</p>
+      <select
+        value={item.qty}
+        onChange={(e) => qtyChangeHandler(item.product, e.target.value)}
+        className="cartItem__select"
+      >
+        {[...Array(item.countInStock).keys()].map((x) => (
+          <option key={x + 1} value={x + 1}>
+            {x + 1}
+          </option>
+        ))}
+      </select>
+      <button
+        className="cartItem__deleteBtn"
+        onClick={() => removeHandler(item.product)}
+      >
         <i className="fas fa-trash"></i>
       </button>
     </div>
-  )
-}
+  );
+};
 
-export default CartItem
+export default CartItem;
